85f37c8f65250cc37fcd0b2874c74ebd95da766f
martijn.slob@algebrakit.nl <martijn.slob@algebrakit.nl>
2022-03-02 20:02:32 +0100

    preview through akit-exercise-preview web component

--- authoring/interaction-editor/interaction-editor.js
+++ authoring/interaction-editor/interaction-editor.js
@@ -47,65 +47,8 @@ function createExercise() {
     // the akit-interaction-editor Web Component displays a GUI to configure a single interaction in the exercise
     let container = document.getElementById('editor-container');
     container.innerHTML = `<akit-interaction-editor ref-id="${refId}" exercise-id="${exerciseId}"></akit-interaction-editor>`;
+    let previewContainer = document.getElementById('preview-container');
+    previewContainer.innerHTML = `<akit-exercise-preview exercise-id="${exerciseId}"></akit-exercise-preview>`;
 }
 
-
-function run() {
-    let spec = exerciseManager.getExercise();
-    console.log(spec);
-    // call /session/create through the secure proxy (which adds the x-api-key header to the request)
-    sendJSON('/algebrakit-secure/session/create', {
-        exercises: [{
-            exerciseSpec: spec
-        }],
-        options: {
-            // let AlgebraKIT automatically infer symbol types (e.g. 'km' is a unit, 'x' is a variable)
-            generateDebugInfo: true
-        },
-        'api-version': 2
-    }).then(resp => {
-        let outputElm = document.getElementById('test-container');
-        if(resp && resp.length==1 &&  resp[0].sessions) {
-            let session = resp[0].sessions[0];
-            if(session.success) {
-                //the given html is optimized such that no roundtrips to the server are required
-                outputElm.innerHTML = session.html;
-                
-                if(session.debugInformation) {
-                    //AlgebraKiT generated information to help authoring. E.g. automatically set symbol types.
-                    //Communicate this information to the authoring component.
-                    exerciseManager.setDebugInfo(session.debugInformation);
-                }
-            } else if(session.errorData) {
-                // the exercise contains some authoring error. Communicate the error data to the authoring 
-                // component such that a meaningful error message can be shown.
-                let editor = document.getElementsByTagName('akit-interaction-editor')[0];
-                editor.setErrorData({
-                    msg: session.msg,
-                    data: session.errorData
-                });
-
-            } else {
-                outputElm.innerHTML = 'An error occurred while trying to create the session'
-            }
-        }
-    });
-}
-
-function sendJSON(url, data) {
-    return new Promise((resolve, reject) => {
-        var json = JSON.stringify(data);
-
-        var xhr = new XMLHttpRequest();
-        xhr.open("POST", url);
-        xhr.setRequestHeader("Content-Type", "application/json");
-        xhr.onreadystatechange = function () {
-            if (xhr.readyState === 4) {
-                resolve(JSON.parse(xhr.responseText));
-            }
-        };
-
-        xhr.send(json);
-    });
-}
 
